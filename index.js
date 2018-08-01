b4bb7235621bb5ef33dc78850caa9699f1839f39
Barak Edry <barak.edry@ebs.com>
2018-08-01 23:14:35 +0300

    basic code of the logger

--- index.js
+++ index.js
@@ -2,3 +2,95 @@
  * Created by barakedry on 01/08/2018.
  */
 'use strict';
+
+const UIActivityLogger = {
+
+    waitingForIdle: false,
+
+    eventsBuffer: [],
+
+    start() {
+        this.worker = new Worker('./worker.js');
+        this.addHandlers();
+    },
+
+    flushAtIdle() {
+        if (this.waitingForIdle) { return; }
+
+        this.waitingForIdle = true;
+        window.requestIdleCallback(() => {
+            this.waitingForIdle = false;
+            this.worker.postMessage({events: this.eventsBuffer});
+            this.eventsBuffer = [];
+        });
+    },
+
+    log(event) {
+        if (this.idleSendTimeoutHandle) {
+            clearTimeout(this.idleSendTimeoutHandle);
+            this.idleSendTimeoutHandle = null;
+        }
+
+        this.eventsBuffer.push(event);
+        this.flushAtIdle();
+    },
+
+    onMouseEvent(event) {
+
+        // defer
+        setTimeout(() => {
+            this.log(event);
+        }, 0);
+    },
+
+    onKeyEvent(e) {
+
+
+        // defer
+        setTimeout(() => {
+            this.log(event);
+        }, 0);
+    },
+
+    send() {
+        this.worker.postMessage('send');
+    },
+
+    addHandlers() {
+        document.addEventListener('click', this.onMouseEvent.bind(this), true);
+        document.addEventListener('keyup', this.onKeyEvent.bind(this), true);
+
+        window.addEventListener('blur', () => {
+            if (this.idleSendTimeoutHandle) {
+                clearTimeout(this.idleSendTimeoutHandle);
+                this.idleSendTimeoutHandle = null;
+            }
+
+            this.idleSendTimeoutHandle = setTimeout(() => {
+                this.send();
+                this.idleSendTimeoutHandle = null;
+            }, 500);
+        });
+
+        window.addEventListener('focus', () => {
+            if (this.idleSendTimeoutHandle) {
+                clearTimeout(this.idleSendTimeoutHandle);
+                this.idleSendTimeoutHandle = null;
+            }
+        });
+
+        window.addEventListener('mouseove', () => {
+            if (this.idleSendTimeoutHandle) {
+                clearTimeout(this.idleSendTimeoutHandle);
+                this.idleSendTimeoutHandle = null;
+            }
+
+            this.idleSendTimeoutHandle = setTimeout(() => {
+                    this.send();
+                this.idleSendTimeoutHandle = null;
+                }, 5000);
+        });
+    }
+};
+
+export default UIActivityLogger;
--- worker.js
+++ worker.js
@@ -2,3 +2,29 @@
  * Created by barakedry on 01/08/2018.
  */
 'use strict';
+
+let events = [];
+
+function compress(events) {
+    return events;
+}
+
+function send(){
+
+    if (events.length === 0) {
+        return;
+    }
+
+    console.log(`sending ${events.length} events`);
+    debugger;
+    events = [];
+
+}
+
+onmessage = function(msg) {
+    if(msg === 'send') {
+        send();
+    } else if (msg.events) (
+        events = events.concat(msg.events)
+    )
+};
