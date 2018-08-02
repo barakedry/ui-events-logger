7f951c38b4c6cdc0769a0d31a544e9697653a4e2
barakedry <bara.edry@ebs.com>
2018-08-02 13:49:08 +0300

    now working

--- worker.js
+++ worker.js
@@ -12,18 +12,33 @@ function compress(events) {
 function formatElementRule(elementProperties) {
     if (!elementProperties) { return; }
 
+    if (elementProperties.isFragment) {
+        return '/deep/'
+    }
+
+    if (elementProperties.type !== 1) {
+        return;
+    }
+
+    if (elementProperties.attributes) {
+        elementProperties.attributes = elementProperties.attributes.filter((attr) => { return 'class id'.indexOf(attr.a) === -1; })
+        if (elementProperties.attributes.length === 0) {
+            delete elementProperties.attributes;
+        }
+    }
+
     return [
         elementProperties.localName,
         elementProperties.id ? '#' + elementProperties.id : '',
         elementProperties.className ? '.' + elementProperties.className.split(' ').join('.') : '',
-        elementProperties.attributes ? elementProperties.attributes.map((attr) => `[${attr}]`) : ''
+        elementProperties.attributes ? elementProperties.attributes.map((attr) => `[${attr.a}${attr.v ? `='${attr.v}'` : ''}]`) : ''
     ].join('');
 }
 
 function formatEvent(event) {
-    event.target = formatElementRule(event.target);
+    event.target = formatElementRule(event.path[0]);
     event.srcElement = formatElementRule(event.srcElement);
-    event.path = event.path.map(formatElementRule);
+    event.path = event.path.map(formatElementRule).filter(i => i).reverse().join(' > ');
     return event;
 }
 
@@ -34,17 +49,16 @@ function send(){
     }
 
     console.log(`sending ${events.length} events`);
-    console.log(events.map(formatEvent));
+    console.log(events.map(formatEvent).map(e => `${e.timeStamp}  ${e.type}@(x=${e.clientX} y=${e.clientY}) on ${e.target} path="${e.path}"`));
 
-    debugger;
     events = [];
 
 }
 
-onmessage = function(msg) {
-    if(msg === 'send') {
+onmessage = function({data}) {
+    if(data === 'send') {
         send();
-    } else if (msg.events) (
-        events = events.concat(msg.events)
+    } else if (data.events) (
+        events = events.concat(data.events)
     )
 };
